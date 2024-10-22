e736f81e963f15d3f45b488b19c1653ecd95614d
Manav Misra <manavm@visionwebsoft.com>
2024-10-22 00:17:32 -0500

    refactor: :recycle: alphabetize

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
-export function formatPrice(price) {
-  return `$${price.toFixed(2)}`;
-}
-
 export function calcPriceWithTax(price, taxRate) {
   return price + price * taxRate;
+}
+
+export function formatPrice(price) {
+  return `$${price.toFixed(2)}`;
 }
